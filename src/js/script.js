43ece3c93946731255be3309712b8b81bdcee4a9
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-05-15 00:06:46 -0300

    Contador adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,43 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function() {
+setInterval(function () {
     nextImage();
 }, 5000)
 
 function nextImage() {
     count++
-    if(count > 4) {
+    if (count > 4) {
         count = 1;
     }
 
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio" + count).checked = true;
 
 }
+
+class Contador {
+    constructor() {
+        this.tempoJuntosDiv = document.querySelector('.tempo-juntos')
+        this.dataInicial = new Date("2024-01-14 15:15").getTime();
+        this.intervalo = null;
+    }
+
+    tempoJuntos() {
+        this.intervalo = setInterval(() => {
+            const dataAtual = new Date().getTime();
+            const subtracao = dataAtual - this.dataInicial;
+            const segundos = Math.floor(subtracao / 1000);
+            const minutos = Math.floor(segundos / 60);
+            const horas = Math.floor(minutos / 60);
+            const dias = Math.floor(horas / 24);
+            const ano = Math.floor(dias / 365);
+
+            const diferenca = (`${ano} ano, ${dias % 365} dias, ${horas % 24} horas, ${minutos % 60} minutos e ${segundos % 60} segundos`)
+
+            this.tempoJuntosDiv.innerHTML = diferenca;
+        }, 1000)   
+    }
+}
+
+const nossoTempoJuntos = new Contador();
+nossoTempoJuntos.tempoJuntos();
