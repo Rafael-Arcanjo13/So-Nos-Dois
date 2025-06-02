6456e3802db1c362d608eb7a5b5dbf278992b4a3
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-06-02 00:26:00 -0300

    Nova lógica para o slide

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-let count = 1;
+/*let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function () {
@@ -13,7 +13,25 @@ function nextImage() {
 
     document.getElementById("radio" + count).checked = true;
 
+}*/
+
+let count = 1;
+let interval = setInterval(nextImage, 5000);
+
+function nextImage() {
+    count++;
+    if (count > 4) count = 1;
+    document.getElementById("radio" + count).checked = true;
 }
+
+// Reinicia o timer quando o usuário clica manualmente nos botões
+document.querySelectorAll(".manual-btn").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        count = index + 1; // Atualiza a contagem com base no botão clicado
+        clearInterval(interval); // Para o antigo
+        interval = setInterval(nextImage, 5000); // Inicia novo
+    });
+});
 
 class Contador {
     constructor() {
